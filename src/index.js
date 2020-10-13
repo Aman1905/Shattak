7ec20d485a03c46b745c41734210f63a58fd59fe
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2020-10-13 19:51:45 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Topbar from './Topbar';
+
+ReactDOM.render(
+  <>
+
+  <Topbar />
+    <App />
+  
+  </>,
+  document.getElementById('root')
+);
+
+
